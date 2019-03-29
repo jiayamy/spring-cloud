5e2a4e9e7a05438db18f4b24097a4f76826d00c2
李嘉 <>
2019-03-29 15:39:59 +0800

    init

---
+++ psp-config-service/src/main/java/org/psp/config/common/Constants.java
@@ -0,0 +1,13 @@
+package org.psp.config.common;
+
+public class Constants {
+
+	public class Env {
+		
+		public static final String DEVELOPMENT = "dev";
+		
+		public static final String PRODUCTION = "prod";
+		
+		public static final String TEST = "test";
+	}
+}
--- psp-eureka-service/src/main/java/org/psp/eureka/App.java
+++ psp-eureka-service/src/main/java/org/psp/eureka/App.java
@@ -1,13 +1,27 @@
 package org.psp.eureka;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class App {
+	
+	Logger logger = LoggerFactory.getLogger(App.class);
+	
+	@PostConstruct
+    public void init() {
+        //初始化可执行代码
+		logger.info("---------init--------");
     }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
 }
